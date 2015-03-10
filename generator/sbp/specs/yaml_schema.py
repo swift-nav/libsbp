02b77bc674b14af9c15e5cc3b0f909caa5d1ad0d
Bhaskar Mookerji <mookerji@gmail.com>
2015-03-10 11:20:42 -0700

    [WIP] Generator for Python and c bindings.
    Includes:
    0. Command-line tool for generating Python bindings from YAML
    specifications.
    1. Uses Python's Construct library for flexible, declarative binary
    parsing.
    2. Basic syntax and schema validation on YAML specifications.
    
    TODO:
    0. Tests and documentation.
    1. Split up templating generation and separate into different files by
    syntax.
    2.
    
    Closes #3, #9, #16.
    
    /cc @fnoble, @mfine

---
+++ generator/sbp/specs/yaml_schema.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# Copyright (C) 2011-2014 Swift Navigation Inc.
+# Contact: Fergus Noble <fergus@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+"""
+Define some basic data shape schema on Python dictionaries parsed
+from Yaml.
+
+"""
+
+from voluptuous import Coerce
+from voluptuous import Optional
+from voluptuous import Schema
+
+SBP_EXTENSION = "/*.yaml"
+
+filename = Schema(str)
+identifier = Schema(str)
+description = Schema(str)
+include = Schema([filename])
+bit = Schema(int)
+type_identifier = Schema(str)
+sbp_identifier = Schema(int)
+units = Coerce(str)
+bitmask = Coerce(str)
+size = Schema(int)
+
+bitfield = Schema([{Optional(bitmask):
+                           {Optional('units'): units,
+                            Optional('values'): [{bit: description}],
+                            Optional('desc'): description}}])
+field = Schema({identifier:
+                {Optional('units'): units,
+                 'type': type_identifier,
+                 Optional('desc'): description,
+                 Optional('map_by'): identifier,
+                 Optional('fill'): identifier,
+                 Optional('size'): size,
+                 Optional('size_fn'): identifier,
+                 Optional('fields'): bitfield}})
+definition = Schema({identifier:
+                     {Optional('id'): sbp_identifier,
+                      Optional('short_desc'): description,
+                      Optional('desc'): description,
+                      Optional('size'): size,
+                      Optional('type'): type_identifier,
+                      Optional('fields'): [field]}})
+package_schema = Schema({Optional('package'): identifier,
+                         Optional('description'): description,
+                         Optional('render_source'): bool,
+                         Optional('stable'): bool,
+                         Optional('include'): include,
+                         Optional('definitions'): [definition]})
