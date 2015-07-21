3cfd0475e1808fd3ca7c6dd8a3a8365fa41ff9ff
Gareth McMullin <gareth@blacksphere.co.nz>
2015-07-20 18:57:05 -0700

    java: update example.
    Use latest log and tracking messages.
    Accept baudrate on the command line.
    Check for bad args and show usage.
    Put timestamp in log file name.

--- java/example/SerialLink.java
+++ java/example/SerialLink.java
@@ -14,13 +14,17 @@ import com.swiftnav.sbp.client.SBPCallback;
 import com.swiftnav.sbp.client.SBPHandler;
 import com.swiftnav.sbp.drivers.SBPDriverJSSC;
 import com.swiftnav.sbp.loggers.JSONLogger;
-import com.swiftnav.sbp.logging.MsgPrintDep;
-import com.swiftnav.sbp.tracking.MsgTrackingStateDepA;
-import com.swiftnav.sbp.tracking.TrackingChannelStateDepA;
+import com.swiftnav.sbp.logging.MsgLog;
+import com.swiftnav.sbp.tracking.MsgTrackingState;
+import com.swiftnav.sbp.tracking.TrackingChannelState;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.lang.Integer;
+import java.lang.NumberFormatException;
+import java.lang.System;
+import java.util.Calendar;
 
 import jssc.SerialPortException;
 
@@ -28,30 +32,52 @@ public class SerialLink {
     private SBPHandler handler;
 
     public static void main(String[] args) {
-        new SerialLink(args[0]);
+        if (args.length == 1) {
+            new SerialLink(args[0]);
+        } else if (args.length == 2) {
+            int baudrate = 0;
+            try {
+                baudrate = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                usage();
+            }
+            new SerialLink(args[0], baudrate);
+        } else {
+            usage();
+        }
+    }
+
+    public static void usage() {
+        System.out.println("usage: SerialLink <port> [baudrate]");
+        System.exit(-1);
     }
 
     SerialLink(String port) {
+        this(port, SBPDriverJSSC.BAUDRATE_DEFAULT);
+    }
+    SerialLink(String port, int baudrate) {
         try {
-            handler = new SBPHandler(new SBPDriverJSSC(port));
+            handler = new SBPHandler(new SBPDriverJSSC(port, baudrate));
         } catch (SerialPortException e) {
             System.err.println("Failed to open serial port: " + e.toString());
-            System.exit(-1);
+            System.exit(-2);
         }
-        handler.addCallback(MsgPrintDep.TYPE, printHandler);
-        handler.addCallback(MsgTrackingStateDepA.TYPE, trackingHandler);
+        handler.addCallback(MsgLog.TYPE, logHandler);
+        handler.addCallback(MsgTrackingState.TYPE, trackingHandler);
         try {
-            handler.addCallback(new JSONLogger(new FileOutputStream("logfile.json")));
+            Calendar cal = Calendar.getInstance();
+            String logfile = String.format("serial-link-%1$tY%1$tm%1$td-%1$tH%1$tM%1$tS.log.json", cal);
+            handler.addCallback(new JSONLogger(new FileOutputStream(logfile)));
         } catch (FileNotFoundException e) {
             System.err.println("Error opening logfile: " + e.toString());
         }
         handler.start();
     }
 
-    SBPCallback printHandler = new SBPCallback() {
+    SBPCallback logHandler = new SBPCallback() {
         @Override
         public void receiveCallback(SBPMessage msg_) {
-            MsgPrintDep msg = (MsgPrintDep)msg_;
+            MsgLog msg = (MsgLog)msg_;
             System.out.print(msg.text);
         }
     };
@@ -59,19 +85,19 @@ public class SerialLink {
     SBPCallback trackingHandler = new SBPCallback() {
         @Override
         public void receiveCallback(SBPMessage msg_) {
-            MsgTrackingStateDepA msg = (MsgTrackingStateDepA)msg_;
+            MsgTrackingState msg = (MsgTrackingState)msg_;
             
             boolean tracking = false;
-            for (TrackingChannelStateDepA state : msg.states)
+            for (TrackingChannelState state : msg.states)
                 if (state.state != 0)
                     tracking = true;
             if (!tracking)
                 return;
 
             System.out.print("Tracking: ");
-            for (TrackingChannelStateDepA state : msg.states) {
+            for (TrackingChannelState state : msg.states) {
                 if (state.state != 0)
-                    System.out.printf("PRN%d(%.1f) ", state.prn + 1, state.cn0);
+                    System.out.printf("PRN%d(%.1f) ", state.sid + 1, state.cn0);
             }
             System.out.println();
         }
