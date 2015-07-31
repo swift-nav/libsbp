44dbca5af90cf1f04a13e8efcd897695fe289c27
Gareth McMullin <gareth@blacksphere.co.nz>
2015-07-31 14:29:18 -0700

    java: First cut at iterator based interface.

--- java/src/com/swiftnav/sbp/client/SBPHandler.java
+++ java/src/com/swiftnav/sbp/client/SBPHandler.java
@@ -11,35 +11,45 @@
  */
 package com.swiftnav.sbp.client;
 
-import com.swiftnav.sbp.SBPBinaryException;
 import com.swiftnav.sbp.SBPMessage;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Arrays;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /** Provide an interface for sending and receiving SBP messages through an
  * SBPDriver.  Implements the details of message framing.
  */
-public class SBPHandler {
-    private static final byte PREAMBLE = 0x55;
-
-    private static final int PREAMBLE_SIZE = 1;
-    private static final int HEADER_SIZE = 5;
-    private static final int CRC_SIZE = 2;
-
+public class SBPHandler implements Iterable<SBPMessage> {
+    private Iterable<SBPMessage> source;
     private static final int THREAD_WAIT_TIMEOUT = 1000;
+    private static final int DEFAULT_QUEUE_SIZE = 0;
 
-    private SBPDriver driver;
-    private HashMap<Integer, LinkedList<SBPCallback>> callbacks;
+    private final HashMap<Integer, LinkedList<WeakReference<SBPCallback>>> callbacks;
     private ReceiveThread receiveThread;
 
-    public SBPHandler(SBPDriver driver_) {
-        driver = driver_;
-        callbacks = new HashMap<Integer, LinkedList<SBPCallback>>();
+    public SBPHandler(Iterable<SBPMessage> source_) {
+        source = source_;
+        callbacks = new HashMap<>();
+    }
+
+    public Iterable<SBPMessage> filter(int id, int queue_size) {
+        return filter(new int[] {id}, queue_size);
+    }
+
+    public Iterable<SBPMessage> filter(int[] ids, int queue_size) {
+        SBPQueueIterator iter = new SBPQueueIterator(queue_size);
+        addCallbackMulti(ids, iter);
+        return iter;
+    }
+
+    @Override
+    public Iterator<SBPMessage> iterator() {
+        return filter(null, DEFAULT_QUEUE_SIZE).iterator();
     }
 
     /** Start the listener/dispatch thread */
@@ -63,81 +73,28 @@ public class SBPHandler {
         receiveThread = null;
     }
 
-    private SBPMessage receive() throws IOException {
-        /* Wait for a preamble to be received */
-        byte[] preamble;
-        do {
-            preamble = driver.read(PREAMBLE_SIZE);
-            if (preamble == null) {
-                throw new IOException("SBPDriver read() returned null!");
-            }
-        } while (preamble[0] != PREAMBLE);
-
-        /* Read the header */
-        byte[] header = driver.read(HEADER_SIZE);
-        int calccrc = CRC16.crc16(header);
-        /* Unpack fields from received header */
-        ByteBuffer bb = ByteBuffer.wrap(header).order(ByteOrder.LITTLE_ENDIAN);
-        int type = bb.getShort() & 0xffff;
-        int sender = bb.getShort() & 0xffff;
-        int len = bb.get() & 0xff;
-
-        byte[] payload = driver.read(len);
-        calccrc = CRC16.crc16(payload, calccrc);
-
-        int crc = ByteBuffer.wrap(driver.read(CRC_SIZE))
-                .order(ByteOrder.LITTLE_ENDIAN).getShort() & 0xffff;
-        if (crc != calccrc) {
-            System.err.println("CRC error in received SBPMessage");
-            return null;
-        }
-
-        return new SBPMessage(sender, type, payload);
-    }
-
-    /** Send an SBPMessage using the driver */
-    public void send(SBPMessage msg) throws IOException {
-        byte[] payload = msg.getPayload();
-        byte[] binmsg = new byte[PREAMBLE_SIZE + HEADER_SIZE + payload.length + CRC_SIZE];
-        ByteBuffer bb = ByteBuffer.wrap(binmsg).order(ByteOrder.LITTLE_ENDIAN);
-        bb.put(PREAMBLE);
-        bb.putShort((short) msg.type);
-        bb.putShort((short) msg.sender);
-        bb.put((byte)payload.length);
-        bb.put(payload);
-        int crc = CRC16.crc16(Arrays.copyOfRange(binmsg, 1, payload.length + 6));
-        bb.putShort((short) crc);
-        driver.write(bb.array());
-    }
-
     /** Register a message handler for a specific message ID. */
     public void addCallback(Integer id, SBPCallback cb) {
-        LinkedList<SBPCallback> cblist;
-        if (callbacks.containsKey(id)) {
-            cblist = callbacks.get(id);
-        } else {
-            cblist = new LinkedList<SBPCallback>();
-            callbacks.put(id, cblist);
+        synchronized (callbacks) {
+            LinkedList<WeakReference<SBPCallback>> cblist;
+            if (callbacks.containsKey(id)) {
+                cblist = callbacks.get(id);
+            } else {
+                cblist = new LinkedList<>();
+                callbacks.put(id, cblist);
+            }
+            cblist.add(new WeakReference<>(cb));
         }
-        cblist.add(cb);
     }
 
     /** Register a message handler for a list of message IDs */
-    public void addCallback(Iterable<Integer> ids, SBPCallback cb) {
+    public void addCallbackMulti(int[] ids, SBPCallback cb) {
+        if (ids == null) {
+            addCallback(null, cb);
+            return;
+        }
         for (int id : ids)
             addCallback(id, cb);
-    }
-
-    /** Register a message handler for all messages */
-    public void addCallback(SBPCallback cb) {
-        LinkedList<SBPCallback> cblist;
-        if (callbacks.containsKey(null)) {
-            cblist = callbacks.get(null);
-        } else {
-            cblist = new LinkedList<SBPCallback>();
-            callbacks.put(null, cblist);
-        }
-        cblist.add(cb);
     }
 
     private class ReceiveThread extends Thread {
@@ -145,32 +102,19 @@ public class SBPHandler {
 
         @Override
         public void run() {
-            while (!stopFlag) {
-                SBPMessage msg;
-                try {
-                    msg = receive();
-                } catch (IOException e) {
-                    System.err.println("SBP Listener thread exiting");
+            for (SBPMessage msg : source) {
+                if (stopFlag)
                     break;
-                }
 
                 if (msg == null)
                     continue;
 
-                try {
-                    msg = MessageTable.dispatch(msg);
-                } catch (SBPBinaryException e) {
-                    System.err.print("Error decoding binary payload");
-                    e.printStackTrace();
-                    continue;
+                synchronized (callbacks) {
+                    if (callbacks.containsKey(msg.type))
+                        dispatch(msg.type, msg);
+                    if (callbacks.containsKey(null))
+                        dispatch(null, msg);
                 }
-
-                if (callbacks.containsKey(msg.type))
-                    for (SBPCallback cb : callbacks.get(msg.type))
-                        cb.receiveCallback(msg);
-                if (callbacks.containsKey(null))
-                    for (SBPCallback cb : callbacks.get(null))
-                        cb.receiveCallback(msg);
             }
         }
 
@@ -178,5 +122,38 @@ public class SBPHandler {
             stopFlag = true;
         }
     }
-}
 
+    private void dispatch(Integer type, SBPMessage msg) {
+        LinkedList<WeakReference<SBPCallback>> cblist = callbacks.get(type);
+        for (WeakReference<SBPCallback> wr : cblist) {
+            SBPCallback cb = wr.get();
+            if (cb != null) {
+                cb.receiveCallback(msg);
+            } else {
+                cblist.remove(wr);
+            }
+        }
+    }
+
+    class SBPQueueIterator extends SBPIterable implements SBPCallback {
+        private BlockingQueue<SBPMessage> queue;
+
+        private SBPQueueIterator(int queue_size) {
+            if (queue_size <= 0) {
+                queue = new LinkedBlockingQueue<>();
+            } else {
+                queue = new ArrayBlockingQueue<>(queue_size);
+            }
+        }
+
+        @Override
+        public void receiveCallback(SBPMessage msg) {
+            queue.add(msg);
+        }
+
+        @Override
+        protected SBPMessage getNext() {
+            return queue.poll();
+        }
+    }
+}
