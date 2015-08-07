9e706da700e6fd7ec5811a7a67ed140c2beffa2a
Gareth McMullin <gareth@blacksphere.co.nz>
2015-08-07 12:08:24 -0700

    java: Allow for strong referenced callbacks.

--- java/src/com/swiftnav/sbp/client/SBPHandler.java
+++ java/src/com/swiftnav/sbp/client/SBPHandler.java
@@ -13,28 +13,31 @@ package com.swiftnav.sbp.client;
 
 import com.swiftnav.sbp.SBPMessage;
 
+import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-/** Provide an interface for sending and receiving SBP messages through an
- * SBPDriver.  Implements the details of message framing.
+/** Provide an interface for queueing and filtering messages.
  */
 public class SBPHandler implements Iterable<SBPMessage> {
     private Iterable<SBPMessage> source;
     private static final int THREAD_WAIT_TIMEOUT = 1000;
     private static final int DEFAULT_QUEUE_SIZE = 0;
 
-    private final HashMap<Integer, LinkedList<WeakReference<SBPCallback>>> callbacks;
+    private final HashMap<Integer, List<Reference<SBPCallback>>> callbacks;
+    private final List<SBPCallback> strongCallbacks;
     private ReceiveThread receiveThread;
 
     public SBPHandler(Iterable<SBPMessage> source_) {
         source = source_;
         callbacks = new HashMap<>();
+        strongCallbacks = new LinkedList<>();
     }
 
     public Iterable<SBPMessage> filter(int id, int queue_size) {
@@ -43,7 +46,7 @@ public class SBPHandler implements Iterable<SBPMessage> {
 
     public Iterable<SBPMessage> filter(int[] ids, int queue_size) {
         SBPQueueIterator iter = new SBPQueueIterator(queue_size);
-        addCallbackMulti(ids, iter);
+        addCallbackMulti(ids, new WeakReference<SBPCallback>(iter));
         return iter;
     }
 
@@ -73,28 +76,56 @@ public class SBPHandler implements Iterable<SBPMessage> {
         receiveThread = null;
     }
 
-    /** Register a message handler for a specific message ID. */
+
     public void addCallback(Integer id, SBPCallback cb) {
         synchronized (callbacks) {
-            LinkedList<WeakReference<SBPCallback>> cblist;
+            addCallback(id, new WeakReference<>(cb));
+            strongCallbacks.add(cb);
+        }
+    }
+
+    /** Register a message handler for a specific message ID. */
+    public void addCallback(Integer id, Reference<SBPCallback> cb) {
+        synchronized (callbacks) {
+            List<Reference<SBPCallback>> cblist;
             if (callbacks.containsKey(id)) {
                 cblist = callbacks.get(id);
             } else {
                 cblist = new LinkedList<>();
                 callbacks.put(id, cblist);
             }
-            cblist.add(new WeakReference<>(cb));
+            cblist.add(cb);
+        }
+    }
+
+    public void addCallbackMulti(int[] ids, SBPCallback cb) {
+        synchronized (callbacks) {
+            addCallbackMulti(ids, new WeakReference<>(cb));
+            strongCallbacks.add(cb);
         }
     }
 
     /** Register a message handler for a list of message IDs */
-    public void addCallbackMulti(int[] ids, SBPCallback cb) {
+    public void addCallbackMulti(int[] ids, Reference<SBPCallback> cb) {
         if (ids == null) {
             addCallback(null, cb);
             return;
         }
         for (int id : ids)
             addCallback(id, cb);
+    }
+
+    public void removeCallback(SBPCallback cb) {
+        synchronized (callbacks) {
+            for (List<Reference<SBPCallback>> cblist : callbacks.values()) {
+                for (Reference<SBPCallback> ref : cblist) {
+                    if (ref.get() == cb) {
+                        cblist.remove(ref);
+                    }
+                }
+            }
+            strongCallbacks.remove(cb);
+        }
     }
 
     private class ReceiveThread extends Thread {
@@ -124,8 +155,8 @@ public class SBPHandler implements Iterable<SBPMessage> {
     }
 
     private void dispatch(Integer type, SBPMessage msg) {
-        LinkedList<WeakReference<SBPCallback>> cblist = callbacks.get(type);
-        for (WeakReference<SBPCallback> wr : cblist) {
+        List<Reference<SBPCallback>> cblist = callbacks.get(type);
+        for (Reference<SBPCallback> wr : cblist) {
             SBPCallback cb = wr.get();
             if (cb != null) {
                 cb.receiveCallback(msg);
