aca23bc5ca6625cf11c9381e1d762ff45e16f092
Mark Fine <mark.fine@gmail.com>
2015-08-11 16:50:32 -0700

    A Skylark Driver.

---
+++ python/sbp/client/drivers/skylark_driver.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2015 Swift Navigation Inc.
+# Contact: Bhaskar Mookerji <mookerji@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+import requests
+
+from .base_driver import BaseDriver
+
+class SkylarkDriver(BaseDriver):
+
+  def __init__(self, device_uid):
+    headers = {'Device-Uid': device_uid, 'Accept': 'application/sbp'}
+    self.response = requests.get('http://skylark.swiftnav.com', stream=True, headers=headers)
+
+  def read(self, size):
+    return self.response.raw.read(size)
