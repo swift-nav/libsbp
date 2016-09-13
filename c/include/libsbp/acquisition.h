145ae67e09e198870478321a8c2ed59d58432292
Dmitry Tatarinov <dmitry.tatarinov@exafore.com>
2016-09-13 11:41:58 +0300

    Add reacq-related SBP message

--- c/include/libsbp/acquisition.h
+++ c/include/libsbp/acquisition.h
@@ -63,6 +63,39 @@ acquisition was attempted
 } msg_acq_result_dep_a_t;
 
 
+/** Acq perfomance measurement and debug
+ *
+ * Profile for a specific SV for debugging purposes
+ * The message describes SV profile during acquisition time.
+ * The message is used to debug and measure the performance.
+ */
+typedef struct __attribute__((packed)) {
+  u8 job_type;      /**< SV search job type (deep, fallback, etc) */
+  u8 status;        /**< Acquisition status 1 is Success, 0 is Failure */
+  u16 cn0;           /**< CN0 value. Only valid if status is '1' [dB-Hz*10] */
+  u8 int_time;      /**< Acquisition integration time [ms] */
+  sbp_gnss_signal_t sid;           /**< GNSS signal for which acquisition was attempted */
+  u16 bin_width;     /**< Acq frequency bin width [Hz] */
+  u32 timestamp;     /**< Timestamp of the job complete event [ms] */
+  u32 time_spent;    /**< Time spent to search for sid.code [us] */
+  u32 cf_min;        /**< Doppler range lowest frequency [Hz] */
+  u32 cf_max;        /**< Doppler range highest frequency [Hz] */
+  u32 cf;            /**< Doppler value of detected peak. Only valid if status is '1' [Hz] */
+  u32 cp;            /**< Codephase of detected peak. Only valid if status is '1' [chips*10] */
+} acq_sv_profile_t;
+
+
+/** Acquisition perfomance measurement and debug
+ *
+ * The message describes all SV profiles during acquisition time.
+ * The message is used to debug and measure the performance.
+ */
+#define SBP_MSG_ACQ_SV_PROFILE   0x001E
+typedef struct __attribute__((packed)) {
+  acq_sv_profile_t acq_sv_profile[0]; /**< SV profiles during acquisition time */
+} msg_acq_sv_profile_t;
+
+
 /** \} */
 
 #endif /* LIBSBP_ACQUISITION_MESSAGES_H */
