110e3d0570c1698ccc88b403ad53d831111ebb1c
Pasi Miettinen <pasi.miettinen@exafore.com>
2017-06-07 16:14:15 +0300

    Remove unnecessary dual packaging which breaks to_binary()

--- python/sbp/utils.py
+++ python/sbp/utils.py
@@ -47,7 +47,7 @@ def walk_json_dict(coll):
     return coll
 
 
-def containerize(coll):
+def containerize(coll, key=None):
   """Walk attribute fields passed from an SBP message and convert to
   Containers where appropriate. Needed for Construct proper
   serialization.
@@ -58,15 +58,10 @@ def containerize(coll):
 
   """
   if isinstance(coll, Container):
-    [setattr(coll, k, containerize(v)) for (k, v) in coll.items()]
+    [setattr(coll, k, containerize(v, k)) for (k, v) in coll.items()]
     return coll
   elif isinstance(coll, dict):
     return containerize(Container(**coll))
-  elif isinstance(coll, list):
-    for j, i in enumerate(coll):
-      if isinstance(i, dict):
-        coll[j] = containerize(Container(**i))
-    return coll
   else:
     return coll
 
