05e5c6594dc2784e82531a4f10161da5dad00c37
dzollo <dzollo@swift-nav.com>
2017-12-11 17:11:38 -0800

    Missing java file

---
+++ java/src/com/swiftnav/sbp/system/MsgInsStatus.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2015 Swift Navigation Inc.
+ * Contact: Gareth McMullin <gareth@swiftnav.com>
+ * Contact: Bhaskar Mookerji <mookerji@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package com.swiftnav.sbp.system;
+
+import java.math.BigInteger;
+
+import com.swiftnav.sbp.SBPMessage;
+import com.swiftnav.sbp.SBPBinaryException;
+import com.swiftnav.sbp.SBPStruct;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+/** SBP class for message MSG_INS_STATUS (0xFF03).
+ *
+ * You can have MSG_INS_STATUS inherent its fields directly from
+ * an inherited SBP object, or construct it inline using a dict of its
+ * fields.
+ *
+ * The INS status message describes the state of the operation
+ * and initialization of the inertial navigation system.  */
+
+public class MsgInsStatus extends SBPMessage {
+    public static final int TYPE = 0xFF03;
+
+    
+    /** Status flags */
+    public long flags;
+    
+
+    public MsgInsStatus (int sender) { super(sender, TYPE); }
+    public MsgInsStatus () { super(TYPE); }
+    public MsgInsStatus (SBPMessage msg) throws SBPBinaryException {
+        super(msg);
+        assert msg.type != TYPE;
+    }
+
+    @Override
+    protected void parse(Parser parser) throws SBPBinaryException {
+        /* Parse fields from binary */
+        flags = parser.getU32();
+    }
+
+    @Override
+    protected void build(Builder builder) {
+        builder.putU32(flags);
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject obj = super.toJSON();
+        obj.put("flags", flags);
+        return obj;
+    }
+}
