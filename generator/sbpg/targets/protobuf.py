a2d83597363599d2ecaad8b2fe3ad04b231817e3
Mark Fine <mark.fine@gmail.com>
2018-03-12 11:48:43 -0700

    Protobuf scaffolding.

---
+++ generator/sbpg/targets/protobuf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 Swift Navigation Inc.
+# Contact: Bhaskar Mookerji <mookerji@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+"""Generator for Protocol Buffers target.
+
+This module consumes the YAML spec and generates some message class
+files.
+
+"""
+
+
+def render_source(output_dir, package_spec):
+    pass
