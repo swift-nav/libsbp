fd7e01809330399f52b8dd6f7f601944dcf307e0
Anthony Cole <anthony@swiftnav.com>
2018-03-19 16:27:57 -0700

    Add generated files

--- haskell/src/SwiftNav/SBP/Msg.hs
+++ haskell/src/SwiftNav/SBP/Msg.hs
@@ -1,8 +1,8 @@
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
-{-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE NoImplicitPrelude           #-}
+{-# LANGUAGE TemplateHaskell             #-}
+{-# LANGUAGE RecordWildCards             #-}
+{-# LANGUAGE OverloadedStrings           #-}
 
 -- |
 -- Module:      SwiftNav.SBP.Msg
@@ -43,9 +43,9 @@ import SwiftNav.SBP.Settings
 import SwiftNav.SBP.Ssr
 import SwiftNav.SBP.System
 import SwiftNav.SBP.Tracking
-import SwiftNav.SBP.Types
 import SwiftNav.SBP.User
 import SwiftNav.SBP.Vehicle
+import SwiftNav.SBP.Types
 
 
 -- | An SBP message ADT composed of all defined SBP messages.
