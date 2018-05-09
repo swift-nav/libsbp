f3d54d2a403d664eaa1cfe0a8a1fd93013d3d40e
Tommi Paakki <tpaakki@exafore.com>
2018-05-09 11:10:59 +0300

    GLO float ephemeris

--- python/tests/sbp/test_table.py
+++ python/tests/sbp/test_table.py
@@ -42,7 +42,7 @@ def test_table_count():
   Test number of available messages to deserialize.
 
   """
-  number_of_messages = 145
+  number_of_messages = 146
   assert len(_SBP_TABLE) == number_of_messages
 
 def test_table_unqiue_count():
