cc5d65db86b8b3d92e254cdef216e7e8c578c24b
dzollo <dzollo@swift-nav.com>
2019-01-16 15:39:49 -0800

    Add additional message to python message count test

--- python/tests/sbp/test_table.py
+++ python/tests/sbp/test_table.py
@@ -44,7 +44,7 @@ def test_table_count():
   Test number of available messages to deserialize.
 
   """
-  number_of_messages = 162
+  number_of_messages = 163
   assert len(_SBP_TABLE) == number_of_messages
 
 def test_table_unqiue_count():
