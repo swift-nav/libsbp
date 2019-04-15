4c8a80e70f2b4440d3ae603f2f634d8d8c1cfc43
Pasi Miettinen <pmiettinen@exafore.com>
2019-04-15 15:19:40 +0300

    Run sbp2prettyjson only if necessary

--- test_data/format-test.sh
+++ test_data/format-test.sh
@@ -7,25 +7,34 @@ fi
 set -e
 
 TESTDATA_ROOT=$(git rev-parse --show-toplevel)/test_data
+INPUT_SHORT=$TESTDATA_ROOT/short.sbp
+OUTPUT_SHORT_HS=$TESTDATA_ROOT/short_hask_pretty.json
+OUTPUT_SHORT_PY=$TESTDATA_ROOT/short_py_pretty.json
+
+INPUT_LONG=$TESTDATA_ROOT/long.sbp
+OUTPUT_LONG_HS=$TESTDATA_ROOT/long_hask_pretty.json
+OUTPUT_LONG_PY=$TESTDATA_ROOT/long_py_pretty.json
 
 PYTHONPATH=$TESTDATA_ROOT/../python/ \
     python $TESTDATA_ROOT/../../piksi_tools/piksi_tools/sbp2json.py \
-    < $TESTDATA_ROOT/short.sbp --mode json --sort-keys --float-meta > $TESTDATA_ROOT/short_py.json
+    < $INPUT_SHORT --mode json --sort-keys --float-meta > $OUTPUT_SHORT_PY
 
-$(find $TESTDATA_ROOT/../haskell/.stack-work/install -name sbp2prettyjson) \
-    < $TESTDATA_ROOT/short.sbp > $TESTDATA_ROOT/short_hask.json
+if [ ! -f $OUTPUT_SHORT_HS ]; then
+  $(find $TESTDATA_ROOT/../haskell/.stack-work/install -name sbp2prettyjson) < $INPUT_SHORT > $OUTPUT_SHORT_HS
+fi
 
-diff $TESTDATA_ROOT/short_hask.json $TESTDATA_ROOT/short_py.json || exit 1
+diff $OUTPUT_SHORT_HS $OUTPUT_SHORT_PY || exit 1
 
 echo -e "Sanity check \e[32mOK\e[0m, please wait for format test.."
 
 PYTHONPATH=$TESTDATA_ROOT/../python/ \
     python $TESTDATA_ROOT/../../piksi_tools/piksi_tools/sbp2json.py \
-    < $TESTDATA_ROOT/long.sbp --mode json --sort-keys --float-meta > $TESTDATA_ROOT/long_py.json
+    < $INPUT_LONG --mode json --sort-keys --float-meta > $OUTPUT_LONG_PY
 
-$(find $TESTDATA_ROOT/../haskell/.stack-work/install -name sbp2prettyjson) \
-    < $TESTDATA_ROOT/long.sbp > $TESTDATA_ROOT/long_hask.json
+if [ ! -f $OUTPUT_LONG_HS ]; then
+  $(find $TESTDATA_ROOT/../haskell/.stack-work/install -name sbp2prettyjson) < $INPUT_LONG > $OUTPUT_LONG_HS
+fi
 
-diff $TESTDATA_ROOT/long_hask.json $TESTDATA_ROOT/long_py.json || exit 1
+diff $OUTPUT_LONG_HS $OUTPUT_LONG_PY || exit 1
 
 echo -e "Format check \e[32mOK\e[0m"
