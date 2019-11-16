d1d6991fb4b4794af3960d2273f9caabe283d290
Joseph Angelo <joseph.angelo@swift-nav.com>
2019-11-15 23:15:18 -0800

    Added a dedicated struct for unknown message types

---
+++ rust/sbp/src/messages/unknown.rs
@@ -0,0 +1,34 @@
+use super::SBPMessage;
+use crate::serialize::SbpSerialize;
+
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+#[derive(Debug)]
+pub struct Unknown {
+    pub msg_id: u16,
+    pub sender_id: u16,
+    pub payload: Vec<u8>,
+}
+
+impl SbpSerialize for Unknown {
+    fn append_to_sbp_buffer(&self, buf: &mut Vec<u8>) {
+        buf.extend(&self.payload);
+    }
+
+    fn sbp_size(&self) -> usize {
+        self.payload.len()
+    }
+}
+
+impl SBPMessage for Unknown {
+    fn get_message_type(&self) -> u16 {
+        self.msg_id
+    }
+
+    fn get_sender_id(&self) -> Option<u16> {
+        Some(self.sender_id)
+    }
+
+    fn set_sender_id(&mut self, new_id: u16) {
+        self.sender_id = new_id;
+    }
+}
