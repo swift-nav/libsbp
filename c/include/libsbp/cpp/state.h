bc32d3e1e493529a6b8e9392275c414f9cf8e7ad
Joseph Angelo <joseph.angelo@swift-nav.com>
2020-02-20 09:04:58 -0800

    Added = delete

--- c/include/libsbp/cpp/state.h
+++ c/include/libsbp/cpp/state.h
@@ -37,9 +37,9 @@ class State {
   IReader *reader_;
   IWriter *writer_;
 
-  State(const State&);
+  State(const State&) = delete;
   
-  State& operator=(const State&);
+  State& operator=(const State&) = delete;
 
   static s32 read_func(u8 *buff, u32 n, void *ctx) {
     State *instance = static_cast<State *>(ctx);
