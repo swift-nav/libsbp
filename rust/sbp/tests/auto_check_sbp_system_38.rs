68b5355eb0eb480f7c14c671292c55974c1957d0
Christian Reimer <christian.reimer@swift-nav.com>
2020-07-24 14:12:35 +0200

    Add untracked files

---
+++ rust/sbp/tests/auto_check_sbp_system_38.rs
@@ -0,0 +1,67 @@
+//
+// Copyright (C) 2019 Swift Navigation Inc.
+// Contact: Swift Navigation <dev@swiftnav.com>
+//
+// This source is subject to the license found in the file 'LICENSE' which must
+// be be distributed together with this source. All other rights reserved.
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+// This file was auto-generated from spec/tests/yaml/swiftnav/sbp/system/test_MsgStartup.yaml by generate.py. Do not modify by hand!
+
+extern crate sbp;
+use sbp::messages::SBPMessage;
+
+mod common;
+#[allow(unused_imports)]
+use common::AlmostEq;
+
+#[test]
+fn test_auto_check_sbp_system_38() {
+    {
+        let payload: Vec<u8> = vec![85, 0, 255, 66, 0, 4, 0, 0, 0, 0, 70, 160];
+
+        // Test the round trip payload parsing
+        let mut parser = sbp::parser::Parser::new();
+        let msg_result = parser.parse(&mut &payload[..]);
+        assert!(msg_result.is_ok());
+        let sbp_msg = msg_result.unwrap();
+        match &sbp_msg {
+            sbp::messages::SBP::MsgStartup(msg) => {
+                assert_eq!(
+                    msg.get_message_type(),
+                    0xff00,
+                    "Incorrect message type, expected 0xff00, is {}",
+                    msg.get_message_type()
+                );
+                let sender_id = msg.get_sender_id().unwrap();
+                assert_eq!(
+                    sender_id, 0x42,
+                    "incorrect sender id, expected 0x42, is {}",
+                    sender_id
+                );
+                assert_eq!(
+                    msg.cause, 0,
+                    "incorrect value for cause, expected 0, is {}",
+                    msg.cause
+                );
+                assert_eq!(
+                    msg.reserved, 0,
+                    "incorrect value for reserved, expected 0, is {}",
+                    msg.reserved
+                );
+                assert_eq!(
+                    msg.startup_type, 0,
+                    "incorrect value for startup_type, expected 0, is {}",
+                    msg.startup_type
+                );
+            }
+            _ => panic!("Invalid message type! Expected a MsgStartup"),
+        };
+
+        let frame = sbp::framer::to_frame(sbp_msg.as_sbp_message()).unwrap();
+        assert_eq!(frame, payload);
+    }
+}
