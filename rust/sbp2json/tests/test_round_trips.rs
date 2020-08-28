5f644cf8f8207e4fd4fba387e01f2a66e01145e9
Jason Mobarak <jason@swiftnav.com>
2020-08-28 14:45:47 -0700

    json2json: maintain fields other than "data" [INFRA-266]
    If we're running json2json and there are fields in the input struct
    other than "data" we need to maintain those fields.  The use case here
    is for HITL, where there's metadata (a "time" field) that needs to
    be kept.

--- rust/sbp2json/tests/test_round_trips.rs
+++ rust/sbp2json/tests/test_round_trips.rs
@@ -1,11 +1,13 @@
 use std::boxed::Box;
+use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
 use std::rc::Rc;
 
 #[macro_use]
 mod common;
 
-use common::{test_round_trip, ThirdTransform};
+use common::{find_project_root, test_round_trip, DeleteTestOutput, ThirdTransform};
 
 use sbp::sbp2json::json2json_read_loop;
 use sbp::sbp2json::json2sbp_read_loop;
@@ -79,4 +81,48 @@ fn test_json2json() -> Result<()> {
         "roundtrip.json2json.input",
         third_transform,
     )
+}
+
+#[test]
+fn test_json2json_maintain_fields() -> Result<()> {
+    let mut del_test_output = DeleteTestOutput::new();
+
+    let root = find_project_root().unwrap();
+    let root = root.as_path();
+
+    let output_path = root.join("test_data/serial-link.log.json.output");
+
+    {
+        let input_path = root.join("test_data/serial-link.log.json");
+        let mut input_file = File::open(input_path).expect("could not open input file");
+
+        del_test_output.add_test_output(&output_path);
+
+        let output_file = File::create(&output_path).expect("could not create output file");
+
+        let mut output_file: Rc<Box<dyn Write>> = Rc::new(Box::new(output_file));
+
+        assert!(json2json_read_loop(false, false, &mut input_file, &mut output_file).is_ok());
+    }
+
+    let mut message_count = 0;
+
+    {
+        let output_file = File::open(output_path).expect("could not open input file");
+        let output_file = BufReader::new(output_file);
+
+        for line in output_file.lines() {
+            let value: serde_json::Value = serde_json::from_str(&line.unwrap()).unwrap();
+            assert!(value.is_object());
+
+            let value = value.as_object().unwrap();
+            assert!(value.contains_key("time"));
+
+            message_count += 1;
+        }
+    }
+
+    assert_eq!(message_count, 10);
+
+    Ok(())
 }
