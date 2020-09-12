b688c5d17dc9bb9db410c90d8489b3f8020d4178
Jason Mobarak <jason@swiftnav.com>
2020-09-12 14:37:06 -0700

    add missing headers

--- java/src/com/swiftnav/sbp/client/SBPFramer.java
+++ java/src/com/swiftnav/sbp/client/SBPFramer.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2020 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 package com.swiftnav.sbp.client;
 
 import com.swiftnav.sbp.SBPBinaryException;
