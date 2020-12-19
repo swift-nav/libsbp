56f307443e4543c7f263d4a2577aad46073ec7eb
Jason Mobarak <jason@swiftnav.com>
2020-12-18 21:52:43 -0800

    drop Python 3.4, add python 3.8

--- python/setup.py
+++ python/setup.py
@@ -26,10 +26,10 @@ CLASSIFIERS = [
   'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
   'Topic :: Software Development :: Libraries :: Python Modules',
   'Programming Language :: Python :: 2.7',
-  'Programming Language :: Python :: 3.4',
   'Programming Language :: Python :: 3.5',
   'Programming Language :: Python :: 3.6',
   'Programming Language :: Python :: 3.7',
+  'Programming Language :: Python :: 3.8',
 ]
 
 PACKAGES = find_packages(exclude=["tests", "bench", "tests.*", "sbp.jit"])
