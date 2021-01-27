7b64d93bf351f881fdd1c037443d5d5afdfc65ad
Steve Meyer <steve.meyer@swift-nav.com>
2021-01-27 13:36:34 -0800

    Replace setup-rust custom action

---
+++ scripts/ci_prepare_rust.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if [ "$RUNNER_OS" == "Linux" ]; then
+    sudo apt-get -qq update
+    sudo apt-get -qq install -y pkg-config build-essential libudev-dev
+elif [ "$RUNNER_OS" == "macOS" ]; then
+    brew install cmake
+elif [ "$RUNNER_OS" == "Windows" ]; then
+    echo "C:\msys64\mingw64\bin" >>$GITHUB_PATH
+else
+    echo "$RUNNER_OS not supported"
+    exit 1
+fi
