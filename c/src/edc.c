a8cb861c86f2c3e2d20167f81d9098b66074edf3
Matt Woodward <matthew.woodward@swift-nav.com>
2021-02-01 12:21:29 +1100

    Common compile flags

--- c/src/edc.c
+++ c/src/edc.c
@@ -74,7 +74,9 @@ static const u16 crc16tab[256] = {
 u16 crc16_ccitt(const u8 *buf, u32 len, u16 crc)
 {
   for (u32 i = 0; i < len; i++)
+  {
     crc = (crc << 8) ^ crc16tab[((crc >> 8) ^ *buf++) & 0x00FF];
+  }
   return crc;
 }
 
