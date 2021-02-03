f66fbcaa53ace0beff0beb1cbefdd2acf645420a
Matt Woodward <matthew.woodward@swift-nav.com>
2021-02-04 09:57:16 +1100

    format

--- c/src/edc.c
+++ c/src/edc.c
@@ -73,8 +73,7 @@ static const u16 crc16tab[256] = {
  */
 u16 crc16_ccitt(const u8 *buf, u32 len, u16 crc)
 {
-  for (u32 i = 0; i < len; i++)
-  {
+  for (u32 i = 0; i < len; i++) {
     crc = (crc << 8) ^ crc16tab[((crc >> 8) ^ *buf++) & 0x00FF];
   }
   return crc;
