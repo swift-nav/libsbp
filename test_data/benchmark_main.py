b94965e0ce32a4fb74546b1eb6dc672c7be04d83
Jason Mobarak <jason@swift-nav.com>
2021-03-19 10:15:18 -0700

    Update benchmark_main.py

--- test_data/benchmark_main.py
+++ test_data/benchmark_main.py
@@ -12,7 +12,7 @@ SLUSH_PERCENTAGE = 0.25
 # How much faster Rust should be than other implementations
 RATIOS_SBP2JSON = {
     "haskell": 2.06,
-    "python": 34.31,
+    "python": 25.32,
 }
 
 RATIOS_JSON2SBP = {
