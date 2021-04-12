bae0fc38f67cbdfa9bd97d08e695cb0388e9a651
Steve Meyer <steve.meyer@swift-nav.com>
2021-04-12 13:40:17 -0700

    Default to unbuffered output

--- rust/sbp2json/src/lib.rs
+++ rust/sbp2json/src/lib.rs
@@ -11,7 +11,7 @@ use sbp::{
     Error, Result,
 };
 
-pub fn json2sbp<R, W>(input: R, output: W, unbuffered: bool) -> Result<()>
+pub fn json2sbp<R, W>(input: R, output: W, buffered: bool) -> Result<()>
 where
     R: Read,
     W: Write,
@@ -19,7 +19,7 @@ where
     let source = FramedRead::new(input, JsonDecoder::new());
     let sink = FramedWrite::new(output, SbpEncoder::new());
 
-    maybe_send_unbuffered(source, sink, unbuffered)?;
+    maybe_send_buffered(source, sink, buffered)?;
 
     Ok(())
 }
@@ -33,12 +33,12 @@ where
     let source = FramedRead::new(input, Json2JsonDecoder {});
     let sink = FramedWrite::new(output, Json2JsonEncoder::new(formatter));
 
-    maybe_send_unbuffered(source, sink, unbufferd)?;
+    maybe_send_buffered(source, sink, unbufferd)?;
 
     Ok(())
 }
 
-pub fn sbp2json<R, W, F>(input: R, output: W, formatter: F, unbuffered: bool) -> Result<()>
+pub fn sbp2json<R, W, F>(input: R, output: W, formatter: F, buffered: bool) -> Result<()>
 where
     R: Read,
     W: Write,
@@ -47,15 +47,15 @@ where
     let source = FramedRead::new(input, SbpDecoder {});
     let sink = FramedWrite::new(output, JsonEncoder::new(formatter));
 
-    maybe_send_unbuffered(source, sink, unbuffered)?;
+    maybe_send_buffered(source, sink, buffered)?;
 
     Ok(())
 }
 
-fn maybe_send_unbuffered<R, W, D, E>(
+fn maybe_send_buffered<R, W, D, E>(
     mut source: FramedRead<R, D>,
     mut sink: FramedWrite<W, E>,
-    unbuffered: bool,
+    buffered: bool,
 ) -> Result<()>
 where
     R: Read,
@@ -63,12 +63,12 @@ where
     D: Decoder<Item = E::Item, Error = Error>,
     E: Encoder<Error = Error>,
 {
-    if unbuffered {
+    if buffered {
+        sink.send_all(source)?;
+    } else {
         while let Some(msg) = source.next() {
             sink.send(msg?)?;
         }
-    } else {
-        sink.send_all(source)?;
     }
 
     Ok(())
