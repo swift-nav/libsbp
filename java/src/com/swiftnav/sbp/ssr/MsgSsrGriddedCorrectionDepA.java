1a6bd4e13fe4b0fd379ceae50c0e685c9c6cea49
Matt Woodward <matthew.woodward@swift-nav.com>
2021-05-21 14:19:55 +1000

    regenerate

--- java/src/com/swiftnav/sbp/ssr/MsgSsrGriddedCorrectionDepA.java
+++ java/src/com/swiftnav/sbp/ssr/MsgSsrGriddedCorrectionDepA.java
@@ -31,10 +31,14 @@ public class MsgSsrGriddedCorrectionDepA extends SBPMessage {
     /** Header of a Gridded Correction message */
     public GriddedCorrectionHeaderDepA header;
     
-    /** Tropo and STEC residuals for the given grid point (mean
-and standard deviation)
- */
-    public GridElement element;
+    /** Index of the grid point */
+    public int index;
+    
+    /** Wet and hydrostatic vertical delays (mean, stddev) */
+    public TroposphericDelayCorrection tropo_delay_correction;
+    
+    /** STEC residuals for each satellite (mean, stddev) */
+    public STECResidual[] stec_residuals;
     
 
     public MsgSsrGriddedCorrectionDepA (int sender) { super(sender, TYPE); }
@@ -48,20 +52,26 @@ and standard deviation)
     protected void parse(Parser parser) throws SBPBinaryException {
         /* Parse fields from binary */
         header = new GriddedCorrectionHeaderDepA().parse(parser);
-        element = new GridElement().parse(parser);
+        index = parser.getU16();
+        tropo_delay_correction = new TroposphericDelayCorrection().parse(parser);
+        stec_residuals = parser.getArray(STECResidual.class);
     }
 
     @Override
     protected void build(Builder builder) {
         header.build(builder);
-        element.build(builder);
+        builder.putU16(index);
+        tropo_delay_correction.build(builder);
+        builder.putArray(stec_residuals);
     }
 
     @Override
     public JSONObject toJSON() {
         JSONObject obj = super.toJSON();
         obj.put("header", header.toJSON());
-        obj.put("element", element.toJSON());
+        obj.put("index", index);
+        obj.put("tropo_delay_correction", tropo_delay_correction.toJSON());
+        obj.put("stec_residuals", SBPStruct.toJSONArray(stec_residuals));
         return obj;
     }
 }
