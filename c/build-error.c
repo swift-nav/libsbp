e01e1df8f43c2d82ac345196e4c2fd97693c1d59
Matt Woodward <matthew.woodward@swift-nav.com>
2021-06-09 12:37:21 +1000

    Add exe which fails to compile

---
+++ c/build-error.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  This will obviously fail to compile, it's here to demonstrate that build errors are being hidden by the C targets in the repo top level makefile
+}
