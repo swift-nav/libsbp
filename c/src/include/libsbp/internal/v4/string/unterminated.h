fe6710be5d25976bf0933fc0ea13cfa55216cd87
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2021-06-10 15:21:09 +1000

    code style

--- c/src/include/libsbp/internal/v4/string/unterminated.h
+++ c/src/include/libsbp/internal/v4/string/unterminated.h
@@ -1,5 +1,17 @@
-#ifndef LIBSBP_INTERNAL_UNPACKED_STRING_UNTERMINATED_H
-#define LIBSBP_INTERNAL_UNPACKED_STRING_UNTERMINATED_H
+/*
+ * Copyright (C) 2021 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swift-nav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef LIBSBP_INTERNAL_V4_STRING_UNTERMINATED_H
+#define LIBSBP_INTERNAL_V4_STRING_UNTERMINATED_H
 
 #include <stdarg.h>
 #include <stdbool.h>
