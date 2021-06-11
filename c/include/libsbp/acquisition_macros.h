be7d878cd7361b62bb1bf543418c945d74a5fa1c
Matt Woodward <matthew.woodward@swift-nav.com>
2021-06-11 16:16:14 +1000

    New generated files to support legacy API changes

---
+++ c/include/libsbp/acquisition_macros.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2015-2021 Swift Navigation Inc.
+ * Contact: https://support.swiftnav.com
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*****************************************************************************
+ * Automatically generated from yaml/swiftnav/sbp/acquisition.yaml
+ * with generate.py. Please do not hand edit!
+ *****************************************************************************/
+
+#ifndef LIBSBP_ACQUISITION_MACROS_H
+#define LIBSBP_ACQUISITION_MACROS_H
+
+#define SBP_MSG_ACQ_RESULT 0x002F
+#define SBP_MSG_ACQ_RESULT_DEP_C 0x001F
+#define SBP_MSG_ACQ_RESULT_DEP_B 0x0014
+#define SBP_MSG_ACQ_RESULT_DEP_A 0x0015
+#define SBP_MSG_ACQ_SV_PROFILE 0x002E
+#define SBP_MSG_ACQ_SV_PROFILE_DEP 0x001E
+
+#endif
---
+++ c/include/libsbp/solution_meta.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2015-2021 Swift Navigation Inc.
+ * Contact: https://support.swiftnav.com
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*****************************************************************************
+ * Automatically generated from yaml/swiftnav/sbp/solution_meta.yaml
+ * with generate.py. Please do not hand edit!
+ *****************************************************************************/
+
+#ifndef LIBSBP_SOLUTION_META_MESSAGES_H
+#define LIBSBP_SOLUTION_META_MESSAGES_H
+
+#include <libsbp/common.h>
+
+SBP_MESSAGE(
+    "Legacy SBP definitions have moved. To continue using legacy message "
+    "definitions include `libsbp/legacy/solution_meta.h` instead. Access to "
+    "legacy types via `libsbp/solution_meta.h` will be removed in version 5.")
+
+#include <libsbp/legacy/solution_meta.h>
+
+#endif /* LIBSBP_SOLUTION_META_MESSAGES_H */
