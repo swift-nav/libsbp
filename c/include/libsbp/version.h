e57c1971d37fe357d6a5d71f7c76d927b5d2c35c
Jason Mobarak <jason@swiftnav.com>
2021-12-10 22:39:31 -0800

    Release v4.1.1

--- c/include/libsbp/version.h
+++ c/include/libsbp/version.h
@@ -25,10 +25,10 @@
 /** Protocol minor version. */
 #define SBP_MINOR_VERSION 1
 /** Protocol patch version. */
-#define SBP_PATCH_VERSION 0
+#define SBP_PATCH_VERSION 1
 
 /** Full SBP version string. */
-#define SBP_VERSION "4.1.1-alpha"
+#define SBP_VERSION "4.1.1"
 
 /** \} */
 
