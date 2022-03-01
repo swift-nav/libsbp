1e16f0ea83b8a80683038e5ec4fc8bac99020988
joelynch <joelynch112@gmail.com>
2022-03-01 15:53:43 +1100

    Release v4.1.4

--- c/include/libsbp/version.h
+++ c/include/libsbp/version.h
@@ -25,10 +25,10 @@
 /** Protocol minor version. */
 #define SBP_MINOR_VERSION 1
 /** Protocol patch version. */
-#define SBP_PATCH_VERSION 3
+#define SBP_PATCH_VERSION 4
 
 /** Full SBP version string. */
-#define SBP_VERSION "4.1.4-alpha"
+#define SBP_VERSION "4.1.4"
 
 /** \} */
 
