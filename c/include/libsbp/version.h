18a95609ddbcb54a45e92b3bd7d3ce148841ea1e
John-Michael Burke <43353147+john-michaelburke@users.noreply.github.com>
2022-04-13 10:14:01 -0700

    Release v4.1.6 (#1115)

--- c/include/libsbp/version.h
+++ c/include/libsbp/version.h
@@ -25,10 +25,10 @@
 /** Protocol minor version. */
 #define SBP_MINOR_VERSION 1
 /** Protocol patch version. */
-#define SBP_PATCH_VERSION 5
+#define SBP_PATCH_VERSION 6
 
 /** Full SBP version string. */
-#define SBP_VERSION "4.1.6-alpha"
+#define SBP_VERSION "4.1.6"
 
 /** \} */
 
