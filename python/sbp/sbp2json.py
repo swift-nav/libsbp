e774ab8f1e1a3e2fed848ab184c9fb8269e691cf
adrian-kong <adrian.k1234567890@gmail.com>
2022-05-09 10:38:28 +1000

    using handler + framer

--- python/sbp/sbp2json.py
+++ python/sbp/sbp2json.py
@@ -2,26 +2,17 @@ from __future__ import print_function
 
 import sys
 
-import io
-
 import json
 
-from construct.core import StreamError
+from sbp.client import Handler, Framer
 
-import sbp.msg
-import sbp.table
-
-from sbp.msg import SBP_PREAMBLE
-from sbp.msg import UnpackError
-
-
-DEFAULT_JSON='rapidjson'
-JSON_CHOICES=['json', 'rapidjson']
+DEFAULT_JSON = 'rapidjson'
+JSON_CHOICES = ['json', 'rapidjson']
 try:
     import rapidjson
 except ImportError:
-    DEFAULT_JSON='json'
-    JSON_CHOICES=['json']
+    DEFAULT_JSON = 'json'
+    JSON_CHOICES = ['json']
 
 try:
     memoryview
@@ -69,56 +60,12 @@ def dump(args, res):
     sys.stdout.write("\n")
 
 
-def sbp_main(args):
-    header_len = 6
-    reader = io.open(args.file.fileno(), 'rb')
-    buf = memoryview(bytearray(4096))
-    unconsumed_offset = 0
-    read_offset = 0
-    buffer_remaining = len(buf)
-    include = set(args.include)
-    while True:
-        if buffer_remaining == 0:
-            buf[0:(read_offset - unconsumed_offset)] = buf[unconsumed_offset:read_offset]
-            read_offset = read_offset - unconsumed_offset
-            unconsumed_offset = 0
-            buffer_remaining = len(buf) - read_offset
-        mv = buf[read_offset:]
-        read_length = reader.readinto(mv)
-        if read_length == 0:
-            unconsumed = read_offset - unconsumed_offset
-            if unconsumed != 0:
-                sys.stderr.write("unconsumed: {}\n".format(unconsumed))
-                sys.stderr.flush()
-            break
-        read_offset += read_length
-        buffer_remaining -= read_length
-        while True:
-            bytes_available = read_offset - unconsumed_offset
-            b = buf[unconsumed_offset:(unconsumed_offset + bytes_available)]
-            if len(b) == 0:
-                break
-            if b[0] != SBP_PREAMBLE:
-                consumed = 1
-            else:
-                try:
-                    m = sbp.msg.SBP.unpack(b)
-                    if not include or m.msg_type in include:
-                        m = sbp.table.dispatch(m)
-                        dump(args, m)
-                    consumed = header_len + m.length + 2
-                except (UnpackError, StreamError):
-                    break
-                except ValueError:
-                    consumed = 1
-            unconsumed_offset += consumed
-
-
 def module_main():
-    args = get_args()
-    if not args:
-        sys.exit(1)
-    try:
-        sbp_main(args)
-    except KeyboardInterrupt:
-        pass
+    if args := get_args():
+        with Handler(Framer(args.file.read, None, verbose=True)) as source:
+            try:
+                it = source.__iter__()
+                while msg_meta := next(it, None):
+                    dump(args, msg_meta[0])
+            except KeyboardInterrupt:
+                pass
