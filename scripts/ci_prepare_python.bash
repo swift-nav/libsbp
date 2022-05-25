1a723e7a8ed4401fcf4982f69f340745689f6f51
Jason Mobarak <jason@swift-nav.com>
2022-05-25 15:42:24 -0700

    add parallel tox, remove python 2.7 support [DEVINFRA-771] (#1152)
    # Description
    
    @swift-nav/devinfra
    
      - We can introduce tox parallel mode to speed up the test-python target in libsbp, but we’ll need to upgrade the GHA job to use Ubuntu 20.04 to get a tox that’s new enough to run it
    
      - (Apparently) this exposes some bugs with Python 2.7 support in Ubuntu, this works locally but could not figure out a fix for GHA… so I propose we try to drop support for this again, Python 2 has been obsolete/discontinued for 2 years now.  Removing support for Python 2.7 ~and Python 3.5~ opens the door for us to introduce type annotations to our Python code.
    
    # API compatibility
    
    _Does this change introduce a API compatibility risk?_  Does not introduce compatibility issues in SBP messages or the API but drops support for Python 2.7 ~and Python 3.5~ (which are both obsolete).
    
    ## API compatibility plan
    
    _If the above is "Yes", please detail the compatibility (or migration) plan:_ This is a "wait and see" compatibility plan, the last customer we had that was using Python 2 was able to migrate to Python 3 after some prodding, so hopefully we'll OK this time around.
    
    # JIRA Reference
    
    https://swift-nav.atlassian.net/browse/DEVINFRA-771

--- scripts/ci_prepare_python.bash
+++ scripts/ci_prepare_python.bash
@@ -3,7 +3,12 @@
 set -ex
 
 sudo apt-get -qq update
-sudo apt-get -y install gcc python2.7 python2.7-dev
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+sudo apt-get -qq update
+
 sudo apt-get install -y \
     build-essential \
     zlib1g-dev \
@@ -17,13 +22,12 @@ sudo apt-get install -y \
     libexpat-dev \
     libpcap-dev \
     liblzma-dev \
-    libpcre3-dev
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y python3.6 python3.6-dev python3.6-distutils \
+    libpcre3-dev \
+    python3.6 python3.6-dev python3.6-distutils \
     python3.7 python3.7-dev python3.7-distutils \
     python3 python3-dev python3-distutils \
     python3.9 python3.9-dev python3.9-distutils \
     python3.10 python3.10-dev python3.10-distutils \
-    tox dpkg-dev
+    tox dpkg-dev wget
+
 pip3 install wheel setuptools
