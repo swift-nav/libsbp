f6ba2366be1365dd933cd1a4056205c21709c497
Jason Mobarak <jason@swiftnav.com>
2022-06-07 17:51:55 -0700

    try adding musl

--- scripts/ci_benchmark.bash
+++ scripts/ci_benchmark.bash
@@ -53,7 +53,7 @@ docker run \
   --rm \
   --name rust-sbp2json-run \
   rust-sbp2json \
-  cargo build --release
+  cargo build --release --target=x86_64-unknown-linux-musl
 
 sudo chown    "$USER:$USER" "$HOME/.docker-cache"
 sudo chown -R "$USER:$USER" "$HOME/.docker-cache/cargo"
