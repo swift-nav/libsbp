d10901f441c608ac693d30756aacd0a6cfb0e4b7
Jason Mobarak <jason@swiftnav.com>
2022-06-07 18:15:31 -0700

    add musl target

--- scripts/ci_prepare_rust.bash
+++ scripts/ci_prepare_rust.bash
@@ -5,6 +5,7 @@ set -ex
 if [ "$RUNNER_OS" == "Linux" ]; then
     sudo apt-get -qq update
     sudo apt-get -qq install -y pkg-config build-essential libudev-dev musl-tools
+    rustup target add x86_64-unknown-linux-musl
 elif [ "$RUNNER_OS" == "macOS" ]; then
     brew install cmake
 elif [ "$RUNNER_OS" == "Windows" ]; then
