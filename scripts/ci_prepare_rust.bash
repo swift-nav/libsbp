6ff7db46db46f942b75a8721341e592708c3035b
Jason Mobarak <jason@swiftnav.com>
2022-06-07 17:53:57 -0700

    Add musl tools

--- scripts/ci_prepare_rust.bash
+++ scripts/ci_prepare_rust.bash
@@ -4,7 +4,7 @@ set -ex
 
 if [ "$RUNNER_OS" == "Linux" ]; then
     sudo apt-get -qq update
-    sudo apt-get -qq install -y pkg-config build-essential libudev-dev
+    sudo apt-get -qq install -y pkg-config build-essential libudev-dev musl-tools
 elif [ "$RUNNER_OS" == "macOS" ]; then
     brew install cmake
 elif [ "$RUNNER_OS" == "Windows" ]; then
