27aac8ef6f124936c9b4918e0b8ef1e02a96622a
Jason Mobarak <jason@swift-nav.com>
2022-08-04 20:59:21 -0700

    update CHANGELOG, prep next release

--- c/include/libsbp/version.h
+++ c/include/libsbp/version.h
@@ -28,7 +28,7 @@
 #define SBP_PATCH_VERSION 0
 
 /** Full SBP version string. */
-#define SBP_VERSION "4.6.0"
+#define SBP_VERSION "4.6.1-alpha"
 
 /** Is this a staging branch? */
 #define SBP_STAGING 0
