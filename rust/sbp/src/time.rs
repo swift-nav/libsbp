2dbb98aac3f9ac29cfb0ce94811c55e74eb77e77
Patrick Crumley <patrick.crumley@swift-nav.com>
2023-05-25 12:17:08 -0700

    adding non-exhaustive for better flexibility in future

--- rust/sbp/src/time.rs
+++ rust/sbp/src/time.rs
@@ -89,10 +89,14 @@ impl From<f64> for RoverTime {
 /// Time received via a message from a base station, like
 /// [MsgObs](crate::messages::observation::MsgObs).
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
-pub struct BaseTime(pub GpsTime);
+#[non_exhaustive]
+pub enum BaseTime {
+    /// The [GpsTime] of the message.
+    GpsTime(GpsTime)
+};
 
 impl From<GpsTime> for BaseTime {
     fn from(gps_time: GpsTime) -> Self {
-        BaseTime(gps_time)
+        BaseTime::GpsTime(gps_time)
     }
 }
