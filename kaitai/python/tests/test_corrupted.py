0170d6124ca7b36e57d8f3db24a95b744db9752b
Daniel Burr <dburr@dburr.net>
2023-05-31 01:45:21 +0200

    NOSONAR

--- kaitai/python/tests/test_corrupted.py
+++ kaitai/python/tests/test_corrupted.py
@@ -12,14 +12,16 @@ def test_corrupted():
         with open("test_data/benchmark.sbp", 'rb') as fp_orig:
             while True:
                 # read random amount
-                buf = fp_orig.read(random.randrange(8192))
+                buf = fp_orig.read(random.randrange(8192)) # NOSONAR
                 if buf is None or len(buf) == 0:
                     break
                 fp_corrupted.write(buf)
-                if random.random() < 0.5: # drop bytes
-                    fp_orig.read(random.randrange(3))
-                else: # write garbage
-                    fp_corrupted.write(bytes(chr(random.randrange(127)), encoding='ascii'))
+                if random.random() < 0.5: # NOSONAR
+                    # drop bytes
+                    fp_orig.read(random.randrange(3)) # NOSONAR
+                else:
+                    # write garbage
+                    fp_corrupted.write(bytes(chr(random.randrange(127)), encoding='ascii')) # NOSONAR
 
     num_messages = compare_parsers(filename_corrupted)
     print("Corrupted test: success, {} messages".format(num_messages))
