688c17defb80d4f5f6829de9ae7426289141214d
Patrick Crumley <patrick.crumley@swift-nav.com>
2023-06-23 11:32:15 -0700

    working on finishing this up

--- rust/sbp2json/src/lib.rs
+++ rust/sbp2json/src/lib.rs
@@ -14,12 +14,28 @@ pub enum ErrorHandlerOptions {
     CoerceToInvalidMsg,
 }
 
-pub fn json2sbp<R, W>(input: R, output: W, buffered: bool, fatal_errors: bool) -> Result<()>
+/// Impl from bool for backwards compatibility
+impl From<bool> for ErrorHandlerOptions {
+    fn from(bool_flag: bool) -> Self {
+        if bool_flag {
+            Self::FailOnError
+        } else {
+            Self::FilterOutErrors
+        }
+    }
+}
+
+pub fn json2sbp<R, W>(
+    input: R,
+    output: W,
+    buffered: bool,
+    error_handler_opt: impl Into<ErrorHandlerOptions>,
+) -> Result<()>
 where
     R: Read,
     W: Write,
 {
-    let source = maybe_fatal_errors(sbp::json::iter_messages(input), fatal_errors);
+    let source = maybe_fatal_errors(sbp::json::iter_messages(input), error_handler_opt.into());
     let mut sink = SbpEncoder::new(output);
     if buffered {
         sink.send_all(source)?;
@@ -31,12 +47,20 @@ where
     Ok(())
 }
 
-pub fn jsonfields2sbp<R, W>(input: R, output: W, buffered: bool, fatal_errors: bool) -> Result<()>
+pub fn jsonfields2sbp<R, W>(
+    input: R,
+    output: W,
+    buffered: bool,
+    error_handler_opt: impl Into<ErrorHandlerOptions>,
+) -> Result<()>
 where
     R: Read,
     W: Write,
 {
-    let source = maybe_fatal_errors(sbp::json::iter_messages_from_fields(input), fatal_errors);
+    let source = maybe_fatal_errors(
+        sbp::json::iter_messages_from_fields(input),
+        error_handler_opt.into(),
+    );
     let mut sink = SbpEncoder::new(output);
     if buffered {
         sink.send_all(source)?;
@@ -53,14 +77,17 @@ pub fn json2json<R, W, F>(
     output: W,
     formatter: F,
     buffered: bool,
-    fatal_errors: bool,
+    error_handler_opt: impl Into<ErrorHandlerOptions>,
 ) -> Result<()>
 where
     R: Read,
     W: Write,
     F: Formatter + Clone,
 {
-    let source = maybe_fatal_errors(sbp::json::iter_json2json_messages(input), fatal_errors);
+    let source = maybe_fatal_errors(
+        sbp::json::iter_json2json_messages(input),
+        error_handler_opt.into(),
+    );
     let mut sink = Json2JsonEncoder::new(output, formatter);
     if buffered {
         sink.send_all(source)?;
@@ -77,14 +104,14 @@ pub fn sbp2json<R, W, F>(
     output: W,
     formatter: F,
     buffered: bool,
-    fatal_errors: bool,
+    error_handler_opt: impl Into<ErrorHandlerOptions>,
 ) -> Result<()>
 where
     R: Read,
     W: Write,
     F: Formatter + Clone,
 {
-    let source = maybe_fatal_errors(sbp::iter_messages(input), fatal_errors);
+    let source = maybe_fatal_errors(sbp::iter_messages(input), error_handler_opt.into());
     let mut sink = JsonEncoder::new(output, formatter);
     if buffered {
         sink.send_all(source)?;
@@ -98,20 +125,24 @@ where
 
 fn maybe_fatal_errors<'a, M, I, E>(
     messages: I,
-    fatal_errors: bool,
+    opts: ErrorHandlerOptions,
 ) -> Box<dyn Iterator<Item = M> + 'a>
 where
     M: 'a,
     I: Iterator<Item = std::result::Result<M, E>> + 'a,
     E: std::error::Error + 'a,
 {
-    if fatal_errors {
-        Box::new(
+    match opts {
+        ErrorHandlerOptions::FailOnError => Box::new(
             messages
                 .take_while(|m| m.as_ref().map_err(|e| eprintln!("{e}")).is_ok())
                 .map(|m| m.unwrap()),
-        )
-    } else {
-        Box::new(messages.filter_map(|m| m.map_err(|e| eprintln!("{e}")).ok()))
+        ),
+        ErrorHandlerOptions::FilterOutErrors => {
+            Box::new(messages.filter_map(|m| m.map_err(|e| eprintln!("{e}")).ok()))
+        }
+        ErrorHandlerOptions::CoerceToInvalidMsg => {
+            todo!("final part of this ticket");
+        }
     }
 }
