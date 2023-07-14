8dcdb16bb0b6996822bb9ab1346590ec2dbfd124
Jason Mobarak <github@jason.mobarak.name>
2023-07-13 20:26:48 -0700

    add docs

--- rust/sbp/src/json/convert.rs
+++ rust/sbp/src/json/convert.rs
@@ -6,6 +6,21 @@ use crate::Sbp;
 
 use super::{JsonError, JsonOutput};
 
+/// A JSON map holding all the fields and metadata of the
+/// SBP message.  This is format used by the output of the
+/// `sbp2json` tool, for example:
+///
+/// {
+///   "crc": 49084,
+///   "length": 52,
+///   "msg_type": 1025,
+///   "msg_name": "MSG_LOG",
+///   "payload": "BHNicCBuZXcgY2xpZW50IGNvbm5lY3Rpb24gOjpmZmZmOjE5Mi4xNjguMC4xMDo2MTQ1MA==",
+///   "preamble": 85,
+///   "sender": 4096,
+///   "level": 4,
+///   "text": "sbp new client connection ::ffff:192.168.0.10:61450"
+/// }
 pub type JsonMap = serde_json::Map<String, serde_json::Value>;
 
 impl<'a> TryFrom<crate::Sbp> for JsonMap
