9721afe29cb4f58985e7ac205075a1eb43cac226
Patrick Crumley <patrick.crumley@swift-nav.com>
2023-08-24 12:24:08 -0700

    Release v5.0.1

--- c/include/libsbp/version.h
+++ c/include/libsbp/version.h
@@ -25,10 +25,10 @@
 /** Protocol minor version. */
 #define SBP_MINOR_VERSION 0
 /** Protocol patch version. */
-#define SBP_PATCH_VERSION 0
+#define SBP_PATCH_VERSION 1
 
 /** Full SBP version string. */
-#define SBP_VERSION "5.0.1-alpha"
+#define SBP_VERSION "5.0.1"
 
 /** Is this a staging branch? */
 #define SBP_STAGING 0
