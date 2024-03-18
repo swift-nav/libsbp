f1cb60348422f8fb538fd671576da236be77132b
Sokhea Ly <sokhealy@gmail.com>
2024-03-18 15:21:28 -0700

    Release v5.0.7

--- c/include/libsbp/version.h
+++ c/include/libsbp/version.h
@@ -25,10 +25,10 @@
 /** Protocol minor version. */
 #define SBP_MINOR_VERSION 0
 /** Protocol patch version. */
-#define SBP_PATCH_VERSION 6
+#define SBP_PATCH_VERSION 7
 
 /** Full SBP version string. */
-#define SBP_VERSION "5.0.7-alpha"
+#define SBP_VERSION "5.0.7"
 
 /** Is this a staging branch? */
 #define SBP_STAGING 0
