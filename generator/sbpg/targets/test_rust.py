f4765bee5da5a881465bb207bc2df6133dc83b91
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2024-08-12 13:21:52 +1000

    update

--- generator/sbpg/targets/test_rust.py
+++ generator/sbpg/targets/test_rust.py
@@ -3,7 +3,7 @@
 # Contact: Swift NAvigation <dev@swiftnav.com>
 #
 # This source is subject to the license found in the file 'LICENSE' which must
-# be be distributed together with this source. All other rights reserved.
+# be distributed together with this source. All other rights reserved.
 #
 # THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
 # EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
