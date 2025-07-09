aac3df7f3c5fc5f9a0cecaf37e2b397699f7f222
Arnaud Mallen <arnaud.mallen@swift-nav.com>
2025-07-09 18:03:35 +0200

    Add something for macos

--- scripts/ci_prepare_rust.bash
+++ scripts/ci_prepare_rust.bash
@@ -6,6 +6,8 @@ if [ "$RUNNER_OS" == "Linux" ]; then
     sudo apt-get -qq update
     sudo apt-get -qq install -y pkg-config build-essential libudev-dev musl-tools
     rustup target add x86_64-unknown-linux-musl
+elif [ "$RUNNER_OS" == "macOS" ]; then
+    echo "Nothing to do for macOS"
 elif [ "$RUNNER_OS" == "Windows" ]; then
     echo "C:\msys64\mingw64\bin" >>"$GITHUB_PATH"
 else
