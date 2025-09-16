bf8aa7d13075cb292551a821df10d18b90e99f5d
Madhunika <madhunika.atmakuri@swift-nav.com>
2025-09-16 13:29:46 -0700

    prep for next release #no_auto_pr

--- c/include/libsbp/version.h
+++ c/include/libsbp/version.h
@@ -28,7 +28,7 @@
 #define SBP_PATCH_VERSION 0
 
 /** Full SBP version string. */
-#define SBP_VERSION "6.3.0"
+#define SBP_VERSION "6.3.1-alpha"
 
 /** Is this a staging branch? */
 #define SBP_STAGING 0
