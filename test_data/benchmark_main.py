76041e50552a549541752d9c5ca996a1f68459c5
𝖉𝖆𝖓𝖎𝖊𝖑 𝖇𝖚𝖗𝖗 <dburr@dburr.net>
2025-10-24 18:34:44 +0200

    Update to kaitaistruct 0.11 (#1518)
    Add support for version 0.11 of the kaitaistruct python runtime.  Starting from v0.11, KaitaiStream::size() calls seek() with whence=SEEK_END to determine the amount of remaining data.  This commit rolls back the changes from cd5403cfd9bf96d67d60ec79afe9a77ed263b0dd and updates the implementation of BufferKaitaiStruct::IOBytes::seek() to support the whence argument.

--- test_data/benchmark_main.py
+++ test_data/benchmark_main.py
@@ -11,10 +11,10 @@ SLUSH_PERCENTAGE = 0.25
 
 # How much faster Rust should be than other implementations
 RATIOS_SBP2JSON = {
-    "haskell": 3.2,
-    "python": 23.38,
+    "haskell": 4.02,
+    "python": 18.7,
     "kaitai_python": 5.00,
-    "kaitai_perl": 20,
+    "kaitai_perl": 24.05,
 }
 
 RATIOS_JSON2SBP = {
@@ -22,7 +22,7 @@ RATIOS_JSON2SBP = {
 }
 
 RATIOS_JSON2JSON = {
-    "haskell": 2.40,
+    "haskell": 3.12,
 }
 
 FAILED = [False]
